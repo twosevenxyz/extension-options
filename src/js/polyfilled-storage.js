e73cb8c06a619edc0f06c07e61e21aa5d71826b1
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2020-02-19 10:23:23 -0500

    Linting fixes

--- src/js/polyfilled-storage.js
+++ src/js/polyfilled-storage.js
@@ -11,6 +11,7 @@ let polyfilledStorage
       this.name = name
       this.storageArea = browser.storage[name]
     }
+
     commonHandle (fn, ...args) {
       return new Promise(function (resolve, reject) {
         fn.call(this.storageArea, ...args, resolve)
@@ -20,15 +21,19 @@ let polyfilledStorage
     get (...args) {
       return this.commonHandle(this.storageArea.get, ...args)
     }
+
     set (...args) {
       return this.commonHandle(this.storageArea.set, ...args)
     }
+
     getBytesInUse () {
       return this.commonHandle(this.storageArea.getBytesInUse)
     }
+
     remove (...args) {
       return this.commonHandle(this.storageArea.remove, ...args)
     }
+
     clear () {
       return this.commonHandle(this.storageArea.clear)
     }
